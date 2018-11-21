e72fd797fedecce9b342e7844118a2e1a0e9b971
jlin03 <jlin03@stuy.edu>
2018-11-21 10:15:16 -0500

    driver

---
+++ Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+  public static void main(String[] args) {
+    MyString x = new MyString("TestString");
+    MyString y = new MyString("StringTest");
+    
+
+
+  }
+
+
+
+}

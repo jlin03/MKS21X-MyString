e3fb08c661f01907179472e938e6639171f9cde4
jlin03 <jlin03@stuy.edu>
2018-11-25 15:39:23 -0500

    quick driver to test

--- Driver.java
+++ Driver.java
@@ -3,8 +3,22 @@ public class Driver {
     MyString x = new MyString("TestString");
     MyString y = new MyString("StringTest");
     
-
-
+	System.out.println(x.length());
+	//10
+	System.out.println(y.length());
+	//10
+	System.out.println(x.subSequence(0,4));
+	//Test
+	System.out.println(y.subSequence(0,6));
+	//String
+	System.out.println(x.charAt(4));
+	//S
+	System.out.println(y.charAt(0));
+	//S
+	System.out.println(x.compareTo(y));
+	//something positive
+	System.out.println(y.compareTo(x));
+	//something negative
   }
 
 
